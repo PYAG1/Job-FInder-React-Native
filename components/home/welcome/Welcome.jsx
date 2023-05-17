88e54bc4c2dd0cdffb8ec18212127f71e468e1b9
PYAG1 <96929032+PYAG1@users.noreply.github.com>
2023-05-17 01:06:02 +0000

    Update Welcome.jsx

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import { useState } from 'react'
-import { View, Text,TextInput,TouchableOpacity,FlatList } from 'react-native'
+import { View, Text,TextInput,TouchableOpacity,FlatList ,Image} from 'react-native'
 
 import styles from './welcome.style'
 import {icons, SIZES} from '../../../constants';
 
-const Welcome = () => {
+
+const Welcome = ({navigation}) => {
   const [search , usesearch] = useState()
+  const [activeJobType,setactiveJobTypes] = useState()
+
+  const jobTypes = ["Full-time", "Part-time", "Contractor"]
+
   return (
     <View>
     <View style={styles.container}>
@@ -22,12 +27,42 @@ const Welcome = () => {
   placeholder='Enter your job'  
   
   />
-  <TouchableOpacity>
-    <Image source={}/>
+  </View>
+  <TouchableOpacity style={styles.searchBtn} onPress={()=>{
+    console.log("apple")
+  }}>
+    <Image source={icons.search}
+    
+    resizeMode='contain'
+    
+    style={styles.searchBtnImage}/>
   </TouchableOpacity>
 
 
+
+
 </View>
+
+<View style={styles.tabsContainer}>
+  <FlatList
+  data={jobTypes}
+  keyExtractor={item=> item}
+  horizontal
+  contentContainerStyle={{columnGap:SIZES.small}}
+  renderItem={({item})=>{
+    return(
+      <TouchableOpacity style={styles.tab(activeJobType, item  )} onPress={()=>{
+      setactiveJobTypes(item)
+      navigation.navigate(`/search/${item}`)
+      }}>
+        <Text style={styles.tabText(activeJobType,item)}>
+          {item}
+        </Text>
+      </TouchableOpacity>
+    )
+  }}
+  />
+  
 
 </View>
     </View>
